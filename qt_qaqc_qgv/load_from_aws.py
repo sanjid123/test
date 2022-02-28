d7135237bbdcd18d1aab6f58b702fc9e2948b578
Nikhil <nkrishnan@aren.ai>
2022-02-28 14:56:11 -0500

    cleared up everything

--- qt_qaqc_qgv/load_from_aws.py
+++ qt_qaqc_qgv/load_from_aws.py
@@ -5,10 +5,25 @@
 import os
 import shutil
 import boto3
+from argparse import ArgumentParser
 import glob
 import time
+import tempfile
+import shutil
+from botocore.exceptions import ClientError
 
-CACHE_NUMBER = 3
+KEY_FILE = "../qt_qaqc_qgv/aws_key.csv"
+with open(KEY_FILE, 'r') as f:
+    lines = f.readlines()
+    key_id = lines[1]
+    key_id = key_id.split(",")
+    key_secret = key_id[3]
+    key_id = key_id[2]
+
+print(key_id)
+print(key_secret)
+
+f.close()
 
 def upload_file(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
@@ -24,7 +39,12 @@ def upload_file(file_name, bucket, object_name=None):
         object_name = os.path.basename(file_name)
 
     # Upload the file
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client(
+        service_name='s3',
+        region_name='us-east-2',
+        aws_access_key_id=key_id,
+        aws_secret_access_key=key_secret
+    )
     try:
         response = s3_client.upload_file(file_name, bucket, object_name)
     except ClientError as e:
@@ -34,126 +54,276 @@ def upload_file(file_name, bucket, object_name=None):
 
 def folder_exists(bucket:str, path:str) -> bool:
     '''
-    Folder should exists. 
+    Folder should exists.
     Folder could be empty.
     '''
     s3 = boto3.client('s3')
-    path = path.rstrip('/') 
+    path = path.rstrip('/')
     resp = s3.list_objects(Bucket=bucket, Prefix=path, Delimiter='/',MaxKeys=1)
     return 'CommonPrefixes' in resp
 
 s3 = boto3.resource(
     service_name='s3',
     region_name='us-east-2',
-    aws_access_key_id='AKIAZGGUW7D4DGBFG446',
-    aws_secret_access_key='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'
+    aws_access_key_id=key_id,
+    aws_secret_access_key=key_secret
 )
 
 s3_client = boto3.client(
     service_name='s3',
     region_name='us-east-2',
-    aws_access_key_id='AKIAZGGUW7D4DGBFG446',
-    aws_secret_access_key='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'
+    aws_access_key_id=key_id,
+    aws_secret_access_key=key_secret
 )
 
-# if os.path.exists("tmp_im"):
-#     shutil.rmtree("tmp_im")
-# if os.path.exists("tmp_mask"):
-#     shutil.rmtree("tmp_mask")
-# os.mkdir("tmp_im")
-# os.mkdir("tmp_mask")
+parser = ArgumentParser()
+parser.add_argument("-i", "--input", dest="user_id",
+                    help="user id", metavar="FILE")
 
-output_masks = {}
+args = parser.parse_args()
+print("arguments are ", args)
 
-curr_ctr = 0
-for obj in s3.Bucket('torngats').objects.all():
-    file_path = obj.key
-    if 'Images' not in file_path:
-        continue
+curr_id = args.user_id
+divided_path = "Tasks/Clean/Divided"
 
-    if ".JPG" not in file_path:
-        continue
+divided_path = divided_path + "/" + curr_id + "/"
+all_tasks = s3.Bucket('torngats').objects.filter(Prefix=divided_path)
 
-    split_path = file_path.split("/")
+if os.path.exists("../qt_qaqc_qgv" + "/" +"tmp_im"):
+    shutil.rmtree("../qt_qaqc_qgv" + "/" +"tmp_im")
+if os.path.exists("../qt_qaqc_qgv" + "/" +"tmp_mask"):
+    shutil.rmtree("../qt_qaqc_qgv" + "/" +"tmp_mask")
+if os.path.exists("../qt_qaqc_qgv" + "/" +"tmp_output"):
+    shutil.rmtree("../qt_qaqc_qgv" + "/" +"tmp_output")
+if os.path.exists("../qt_qaqc_qgv" + "/" +"tasks"):
+    shutil.rmtree("../qt_qaqc_qgv" + "/" +"tasks")
+if os.path.exists("../qt_qaqc_qgv" + "/" +"tasks_complete"):
+    shutil.rmtree("../qt_qaqc_qgv" + "/" +"tasks_complete")
 
-    curr_path = ""
-    ctr = 0
-    for folder in split_path:
-        if ctr == 0:
-            ctr += 1
+os.mkdir("../qt_qaqc_qgv" + "/" +"tmp_im")
+os.mkdir("../qt_qaqc_qgv" + "/" +"tmp_mask")
+os.mkdir("../qt_qaqc_qgv" + "/" +"tmp_output")
+os.mkdir("../qt_qaqc_qgv" + "/" +"tasks")
+os.mkdir("../qt_qaqc_qgv" + "/" +"tasks_complete")
+
+mask_dict = {}
+im_dict = {}
+
+prev_text = ""
+download_file = True
+while(1):
+    for object_summary in all_tasks:
+        key = object_summary.key
+        if ".txt" not in key:
             continue
 
-        if '.JPG' in folder:
-            continue
+        key_split = key.split("/")
 
-        curr_path += folder
-        curr_path += "/"
+        # tmp = tempfile.NamedTemporaryFile()
 
-    curr_camera = split_path[2]
-    curr_camera = curr_camera.replace(" ", "")
+        if download_file:
+            if key_split[-1] == prev_text:
+                continue
 
-    im_name = split_path[-1]
-    im_name = im_name[:im_name.rfind(".")]
+            txt_file = "../qt_qaqc_qgv" + "/" + "tasks/" + key_split[-1]
+            prev_text = key_split[-1]
 
-    curr_path += im_name
+            tmp_txt = key_split[-1]
+            s3_client.download_file('torngats', key, tmp_txt)
+            download_file = False
 
-    mask_path = "Masks/" + curr_path + ".png"
-    im_path = "Images/" + curr_path + ".JPG"
-    cleaned_path = "Cleaned_Status/" + curr_path + ".txt"
+            with open(txt_file, 'w') as f_write:
+                with open(tmp_txt, 'r') as f_read:
+                    for line in f_read.readlines():
+                        line = line.strip()
+                        line_split = line.split("/")
+                        dataset = line_split[1]
+                        camera = line_split[2]
+                        im_name = line_split[3][:line_split[3].find(".")]
+                        f_write.write(camera.replace(" ", "") + "_" + im_name + "\n")
 
-    try:
-        s3_client.head_object(Bucket='torngats', Key=cleaned_path)
-        continue
-    except:
+            f_write.close()
+    
+            # with open(txt_file, 'w') as f_write:
+            with open(tmp_txt, 'r') as f_read:
+                for line in f_read.readlines():
+                    line = line.strip()
+                    line_split = line.split("/")
+                    dataset = line_split[1]
+                    camera = line_split[2]
+                    im_name = line_split[3][:line_split[3].find(".")]
+
+                    im_path = line
+                    mask_path = "Masks/" + dataset + "/" + camera + "/" + im_name + ".png"
+                    
+                    local_im_path = "../qt_qaqc_qgv" + "/" + "tmp_im/" + camera.replace(" ", "") + "_" + im_name + ".jpg"
+                    local_mask_path = "../qt_qaqc_qgv" + "/" + "tmp_mask/" + camera.replace(" ", "") + "_" + im_name + ".png"
+
+                    s3_client.download_file('torngats', mask_path, local_mask_path)
+                    s3_client.download_file('torngats', im_path, local_im_path)
+                    # f_write.write(camera.replace(" ", "") + "_" + im_name + "\n")
+
+                    mask_dict[camera.replace(" ", "") + "_" + im_name] = mask_path
+                    im_dict[camera.replace(" ", "") + "_" + im_name] = im_path
+
+                    filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tasks_complete" + "/*.txt")
+                    remove_indices = []
+                    i = 0
+
+                    # tasks_filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tasks" + "/*.txt")
+
+                    for filename in filenames:
+                        with open(filename, 'a') as f:
+                            f.write("Labeler: " + curr_id)
+                        f.close()
+
+                        finished_path = "Tasks/Clean/Complete/" + filename[filename.rfind("/")+1:]
+                        old_path = "Tasks/Clean/Divided/" + curr_id + "/" + filename[filename.rfind("/")+1:]
+                        # os.remove(filename)
+                        remove_indices.append(i)
+                        i += 1
+                        upload_file(filename, 'torngats', object_name=finished_path)
+                        s3.Object('torngats', old_path).delete()
+                    for index in remove_indices:
+                        os.remove(filenames[index])
+
+                    filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tmp_output" + "/*.png")
+                    if len(filenames) == 0:
+                        pass
+                        # print("waiting for output...")
+                    else:
+                        for filename in filenames:
+                            split_filename = filename.split("/")
+
+                            im_name = split_filename[3][:split_filename[3].rfind(".")]
+                            mask_upload_path = mask_dict[im_name]
+
+                            mask_upload_path_split = mask_upload_path.split("/")
+
+                            upload_file(filename, curr_id.lower(), mask_upload_path)
+                            im_path = "../qt_qaqc_qgv" + "/" + "tmp_im/" + im_name + ".jpg"
+                            mask_path = "../qt_qaqc_qgv" + "/" + "tmp_mask/" + im_name + ".png"
+                            output_path = "../qt_qaqc_qgv" + "/" + "tmp_output/" + im_name + ".png"
+
+                            # time.sleep(1)
+
+                            os.remove(im_path)
+                            os.remove(mask_path)
+                            os.remove(output_path)
+
+                            f_read.close()
+                            # f_write.close()
+
+    filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tmp_output" + "/*.png")
+    if len(filenames) == 0:
         pass
+        # print("waiting for output...")
+    else:
+        for filename in filenames:
+            split_filename = filename.split("/")
 
-    with open('tmp_im.txt', 'w') as f:
-        f.write("Active")
+            im_name = split_filename[3][:split_filename[3].rfind(".")]
+            mask_upload_path = mask_dict[im_name]
 
-    # upload_file('tmp_im.txt', 'torngats', object_name=cleaned_path)
+            upload_file(filename, curr_id.lower(), mask_upload_path)
+            im_path = "../qt_qaqc_qgv" + "/" + "tmp_im/" + im_name + ".jpg"
+            mask_path = "../qt_qaqc_qgv" + "/" + "tmp_mask/" + im_name + ".png"
+            output_path = "../qt_qaqc_qgv" + "/" + "tmp_output/" + im_name + ".png"
 
-    s3_client.download_file('torngats', mask_path, "tmp_mask/" + curr_camera + "_" + im_name + ".png")
-    s3_client.download_file('torngats', im_path, "tmp_im/" + curr_camera + "_" + im_name + ".png")
+            # time.sleep(1)
 
-    output_path = "Masks_Clean/" + curr_path + ".png"
-    output_masks["tmp_output/" + curr_camera + "_" + im_name + ".png"] = output_path
+            os.remove(im_path)
+            os.remove(mask_path)
+            os.remove(output_path)
 
-    curr_ctr += 1
+    filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tmp_im" + "/*.jpg")
+    
+    tasks_filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tasks" + "/*.txt")
+    if not len(tasks_filenames) >= 2:
+        if len(filenames) < 3:
+            # print("Next Task!")
+            download_file = True
+            continue
 
-    print(output_masks)
-    print(curr_ctr)
-
-    if curr_ctr < CACHE_NUMBER:
-        continue
-
-    filenames = glob.glob("tmp_output" + "/*.png")
-    print(output_masks)
-
+    filenames = glob.glob("../qt_qaqc_qgv" + "/" + "tasks_complete" + "/*.txt")
+    remove_indices = []
+    i = 0
+    # if len(filenames) > 0:
+    #     time.sleep(3)
     for filename in filenames:
-        curr_upload_mask = output_masks[filename]
-        # upload_file(filename, 'torngats', object_name=curr_upload_mask)
-        im_name = filename[filename.find("/")+1:filename.rfind(".")]
-        del output_masks[filename]
+        with open(filename, 'a') as f:
+            f.write("Labeler: " + curr_id)
+        f.close()
 
-        os.remove("tmp_im/" + im_name + ".png")
-        os.remove("tmp_mask/" + im_name + ".png")
-        os.remove("tmp_output/" + im_name + ".png")
-
-    while(1):
-        filenames = glob.glob("tmp_output" + "/*.png")
-        if len(filenames) > 0:
-            break
-        time.sleep(5)
-
+        finished_path = "Tasks/Clean/Complete/" + filename[filename.rfind("/")+1:]
+        old_path = "Tasks/Clean/Divided/" + curr_id + "/" + filename[filename.rfind("/")+1:]
+        # os.remove(filename)
+        remove_indices.append(i)
+        i += 1
+        upload_file(filename, 'torngats', object_name=finished_path)
+        s3.Object('torngats', old_path).delete()
+    for index in remove_indices:
+        os.remove(filenames[index])
 
 
-    # break
+    
+    time.sleep(5)
+
+#curr_ctr = 0
+#for obj in s3.Bucket('torngats').objects.all():
+#    file_path = obj.key
+#    if 'Images' not in file_path:
+#        continue
+
+#    if ".JPG" not in file_path:
+#        continue
+
+#    split_path = file_path.split("/")
+
+#    curr_path = ""
+#    ctr = 0
+#    for folder in split_path:
+#        if ctr == 0:
+#            ctr += 1
+#            continue
+
+#        if '.JPG' in folder:
+#            continue
+
+#        curr_path += folder
+#        curr_path += "/"
+
+#    curr_camera = split_path[2]
+#    curr_camera = curr_camera.replace(" ", "")
+
+#    im_name = split_path[-1]
+#    im_name = im_name[:im_name.rfind(".")]
+
+#    curr_path += im_name
+
+#    mask_path = "Masks/" + curr_path + ".png"
+#    im_path = "Images/" + curr_path + ".JPG"
+#    cleaned_path = "Cleaned_Status/" + curr_path + ".txt"
+
+#    try:
+#        s3_client.head_object(Bucket='torngats', Key=cleaned_path)
+#        continue
+#    except:
+#        pass
+
+#    with open('tmp_im.txt', 'w') as f:
+#        f.write("Active")
+
+#    upload_file('tmp_im.txt', 'torngats', object_name=cleaned_path)
+
+#    s3_client.download_file('torngats', mask_path, "tmp_mask/" + curr_camera + "_" + im_name + ".png")
+#    s3_client.download_file('torngats', im_path, "tmp_im/" + curr_camera + "_" + im_name + ".png")
 
     # curr_ctr += 1
 
     # if curr_ctr <= 5:
 
-    
+
 
 # while (1):
     # input = "tmp_output"

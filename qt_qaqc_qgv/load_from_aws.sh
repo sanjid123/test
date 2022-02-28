573d365a639acbad7284a861491b64460642b558
Nikhil <nkrishnan@aren.ai>
2022-02-28 15:58:50 -0500

    UPDATE

--- qt_qaqc_qgv/load_from_aws.sh
+++ qt_qaqc_qgv/load_from_aws.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 echo "UserId: $1";
-"conda init bash";
-"conda activate";
-"conda activate py3.9";
+conda init bash;
+conda activate
+conda activate py3.9;
 echo $PATH;
 python3 ../qt_qaqc_qgv/load_from_aws.py --input $1;
